04f5429a92f109e7050d80367200965386749fa1
AbdAlrahman13 <blalabo89@gmail.com>
2025-09-17 10:36:34 +0300

    commit

--- src/api/configAxios.js
+++ src/api/configAxios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://ecommerce-back-4.onrender.com",
-  baseURL: "http://localhost:7000",
+  baseURL: "https://alomra-website-backend.onrender.com",
+  // baseURL: "http://localhost:7000",
 });
 
 export default axiosInstance;
