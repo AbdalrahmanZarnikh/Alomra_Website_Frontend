c2cdbfbfe3d4b28d97de3d2e3d6b5f4ba09c76f7
AbdAlrahman13 <blalabo89@gmail.com>
2025-10-03 19:07:59 +0300

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,7 +110,7 @@ function Home() {
 
           {/* Start Head Section  */}
 
-          <h1 className="flex justify-center mb-7 text-3xl md:text-4xl ">
+          <h1 className="flex justify-center mb-10 text-2xl md:text-4xl mt-5 ">
             عمرة
             <span className="mx-2  text-[#FF8D4C]/90 font-bold">{omra}</span>
           </h1>
