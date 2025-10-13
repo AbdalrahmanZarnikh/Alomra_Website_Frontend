a610cd40729e8a924d5d018aa2551f8c2145d773
AbdAlrahman13 <blalabo89@gmail.com>
2025-10-13 09:33:09 +0300

    commit

--- src/components/FormCommon/ButtonForm/ButtonFrom.jsx
+++ src/components/FormCommon/ButtonForm/ButtonFrom.jsx
@@ -2,7 +2,7 @@
 const ButtonFrom = ({id,isLoading}) => {
 
   return (
-    <button type="submit" className="submit-button m-auto cursor-pointer">
+    <button type="submit" className="submit-button m-auto cursor-pointer bg-primary">
       {typeof id == "string" ? "تعديل" : "اضافة"}
       {isLoading === "Pending" ? "..." : ""}
     </button>
