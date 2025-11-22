81c78ae0d8e68e8da805d5429bea7d4fd937b19f
AbdalrahmanZarnikh <blalabo89@gmail.com>
2025-11-21 22:39:31 -0800

    commit

--- src/components/Seat/Seat.jsx
+++ src/components/Seat/Seat.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Seat = ({number,person}) => {
   return (
     <div className="border rounded-xl p-2 w-60 h-20 flex flex-col items-center font-bold justify-center text-lg shadow">
-      <p className="text-xl">{number}</p>
+      <p className="text-xl text-primary">{number}</p>
       <p>{person}</p>
     </div>
   )
